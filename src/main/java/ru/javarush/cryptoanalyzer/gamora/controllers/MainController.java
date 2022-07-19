a1e028434e6b1ebced9c74b0196a0fc18b669b96
some-russian <65742649+some-russian@users.noreply.github.com>
2022-07-19 20:23:58 +0300

    Создан аппликейшон

--- src/main/java/ru/javarush/cryptoanalyzer/gamora/controllers/MainController.java
+++ src/main/java/ru/javarush/cryptoanalyzer/gamora/controllers/MainController.java
@@ -1,15 +1,15 @@
 package ru.javarush.cryptoanalyzer.gamora.controllers;
 
 import ru.javarush.cryptoanalyzer.gamora.actions.Action;
-import ru.javarush.cryptoanalyzer.gamora.actions.Actions;
+import ru.javarush.cryptoanalyzer.gamora.actions.ActionFactory;
 import ru.javarush.cryptoanalyzer.gamora.entity.Result;
 
 public class MainController implements Controller {
 
     public Result execute(String command, String[] parameters) {
-        Action action = Actions.find(command);
-        throw new UnsupportedOperationException();
-        //return action.execute(parameters);
+        ActionFactory actionFactory = new ActionFactory();
+        Action action = actionFactory.getAction(parameters);
+        return action.execute();
     }
 
 }
